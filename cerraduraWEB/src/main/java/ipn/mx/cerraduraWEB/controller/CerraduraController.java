9bf4272a018ef52503d6fefdf3e173cf0daba77a
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2024-09-22 12:23:30 -0600

    implementacion thymelaf

--- cerraduraWEB/src/main/java/ipn/mx/cerraduraWEB/controller/CerraduraController.java
+++ cerraduraWEB/src/main/java/ipn/mx/cerraduraWEB/controller/CerraduraController.java
@@ -1,5 +1,13 @@
 package ipn.mx.cerraduraWEB.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CerraduraController {
-    //Lo que va a resolver
+    @GetMapping ("/")
+    public String index(Model model) {
+        return "index";
+}
 }
