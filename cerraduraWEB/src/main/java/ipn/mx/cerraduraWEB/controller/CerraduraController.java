86f94e7ce06db2288de58b4fd478e77c58873330
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2024-09-22 21:11:24 -0600

    index

--- cerraduraWEB/src/main/java/ipn/mx/cerraduraWEB/controller/CerraduraController.java
+++ cerraduraWEB/src/main/java/ipn/mx/cerraduraWEB/controller/CerraduraController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CerraduraController {
     @GetMapping ("/")
     public String index(Model model) {
-        model.addAttribute("message", "Welcome to Thymeleaf with Spring Boot!");
         return "index";
 }
 }
