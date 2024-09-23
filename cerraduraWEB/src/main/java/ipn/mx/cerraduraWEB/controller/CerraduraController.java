a6c5f0829bed992ab3aadbaf0a03748cff25e054
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2024-09-23 06:01:02 -0600

    post

--- cerraduraWEB/src/main/java/ipn/mx/cerraduraWEB/controller/CerraduraController.java
+++ cerraduraWEB/src/main/java/ipn/mx/cerraduraWEB/controller/CerraduraController.java
@@ -1,13 +1,30 @@
 package ipn.mx.cerraduraWEB.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class CerraduraController {
+
+  
     @GetMapping ("/")
     public String index(Model model) {
-        return "index";
+        model.addAttribute("operacion",new Operacion());
+                return "index";
 }
+      @PostMapping("/operaciones")
+    public String operar(@ModelAttribute Operacion operacion, Model model ){
+        String cadena = operacion.getCadena();
+        Integer longitud = operacion.getLongitud();
+
+        String resultado = "Resultado de la cerradura de Kleene para la cadena: " + cadena + " con longitud: " + longitud;
+
+        model.addAttribute("resultado", resultado);
+        return "index"; 
+    }  
 }
---
+++ cerraduraWEB/src/main/java/ipn/mx/cerraduraWEB/controller/Operacion.java
@@ -0,0 +1,24 @@
+package ipn.mx.cerraduraWEB.controller;
+
+public class Operacion {
+    private String cadena;
+    private int longitud;
+
+    public String getCadena() {
+        return cadena;
+    }
+
+    public void setCadena(String cadena) {
+        this.cadena = cadena;
+    }
+
+    public Integer getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(Integer longitud) {
+        this.longitud = longitud;
+    }
+
+    
+}
